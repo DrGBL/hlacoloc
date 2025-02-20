97b2be1d7d7b053e56118fb90a17522a27d68bd0
Guillaume Butler-Laporte <guillaume_bl@hotmail.com>
2025-02-20 16:39:08 -0500

    Update hla_coloc.R

--- R/hla_coloc.R
+++ R/hla_coloc.R
@@ -133,7 +133,7 @@ hla_coloc<-function(pheno1,pheno1R,is_cohort_ld_pheno1=FALSE,
           dplyr::bind_rows(posteriors_fit,.)
 
         bayes_corr_df<-data.frame(gene=g,
-                                  odds_p_map=1/p_map(bayes_lm)$p_MAP[2],
+                                  odds_p_map=1/bayestestR::p_map(bayes_lm)$p_MAP[2],
                                   direction_of_correlation=ifelse(bayes_lm$coefficients[2] >= 0,
                                                                   "Correct",
                                                                   "Incorrect")) %>%
