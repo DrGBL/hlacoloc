0d48c893b478f8b2266944616974a9b7d5b10bbb
Guillaume Butler-Laporte <guillaume_bl@hotmail.com>
2025-03-05 11:43:34 -0500

    Update hla_coloc.R

--- R/hla_coloc.R
+++ R/hla_coloc.R
@@ -206,7 +206,7 @@ hla_coloc<-function(pheno1,pheno1R,is_cohort_ld_pheno1=FALSE,
         annotate_df<-data.frame(gene=g,
                                 x=0.25,
                                 y=0.75,
-                                text=paste0("P_coloc: ", format(full_final_summary %>%
+                                text_tmp=paste0("P_coloc: ", format(full_final_summary %>%
                                                                   dplyr::filter(.data$gene==g) %>%
                                                                   dplyr::pull(.data$hla_colocalization_probability), digits=2))) %>%
           dplyr::bind_rows(annotate_df,)
@@ -215,11 +215,11 @@ hla_coloc<-function(pheno1,pheno1R,is_cohort_ld_pheno1=FALSE,
       }
 
       annotate_df<-annotate_df %>%
-        dplyr::mutate(text_tmp=ifelse(is.na(text_tmp),0,text_tmp)) %>%
-        dplyr::mutate(text_tmp=label_percent()(text_tmp)) %>%
-        dplyr::mutate(text=paste0("P_coloc: ", text_tmp)) %>%
-        dplyr::select(-text_tmp) %>%
-        dplyr::mutate(text=paste0(stringr::str_extract(text,"P_coloc: [0-9]*\\.[0-9]"),"%"))
+        dplyr::mutate(.data$text_tmp=ifelse(is.na(.data$text_tmp),0,text_tmp)) %>%
+        dplyr::mutate(.data$text_tmp=label_percent()(.data$text_tmp)) %>%
+        dplyr::mutate(.data$text=paste0("P_coloc: ", .data$text_tmp)) %>%
+        dplyr::select(-.data$text_tmp) %>%
+        dplyr::mutate(.data$text=paste0(stringr::str_extract(.data$text,"P_coloc: [0-9]*\\.[0-9]"),"%"))
 
       pip_coloc<-full_final %>%
         ggplot2::ggplot(ggplot2::aes(x=.data$pip_pheno1,y=.data$pip_pheno2))+
