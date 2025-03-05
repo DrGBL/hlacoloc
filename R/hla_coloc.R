294ff845dd14e4a7b09bd9f7cbdb7c065f9ff5ab
Guillaume Butler-Laporte <guillaume_bl@hotmail.com>
2025-03-05 11:58:38 -0500

    Update hla_coloc.R

--- R/hla_coloc.R
+++ R/hla_coloc.R
@@ -216,7 +216,7 @@ hla_coloc<-function(pheno1,pheno1R,is_cohort_ld_pheno1=FALSE,
 
       annotate_df<-annotate_df %>%
         dplyr::mutate(text_tmp=ifelse(is.na(.data$text_tmp),0,.data$text_tmp)) %>%
-        dplyr::mutate(text_tmp=label_percent()(.data$text_tmp)) %>%
+        dplyr::mutate(text_tmp=scales::label_percent()(.data$text_tmp)) %>%
         dplyr::mutate(text=paste0("P_coloc: ", .data$text_tmp)) %>%
         dplyr::select(-.data$text_tmp) %>%
         dplyr::mutate(text=paste0(stringr::str_extract(.data$text,"P_coloc: [0-9]*\\.[0-9]"),"%"))
